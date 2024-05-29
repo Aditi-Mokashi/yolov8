42b64fba19ec2d810a5ab433b57ed4549a4bc048
Aditi-Mokashi <aditimokashi23@gmail.com>
2024-05-30 01:35:12 +0530

    fix: store video in storage and get url in realtime db

--- db_utils.py
+++ db_utils.py
@@ -2,64 +2,45 @@ import firebase_admin
 from firebase_admin import credentials, firestore, db, storage
 import re
 
+
 def insert_to_realtime_db(email: str):
-    try:
-        cred = credentials.Certificate("serviceAccountsKey.json")
-        firebase_admin.initialize_app(cred, {
-            'databaseURL': 'https://eqw-violationeye-42382-default-rtdb.firebaseio.com/',
-            'storageBucket': 'eqw-violationeye-42382.appspot.com'
-        })
+    cred = credentials.Certificate("serviceAccountsKey.json")
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://eqw-violationeye-42382-default-rtdb.firebaseio.com/',
+        'storageBucket': 'eqw-violationeye-42382.appspot.com'
+    })
 
-        # cred = credentials.Certificate("path/to/serviceAccountKey.json")
-        # firebase_admin.initialize_app(cred)
+    with open('output.txt', 'r') as file:
+        data = file.read()
+        number_plate = re.findall('[A-Z]{2}\\d{2}[A-Z]{2}\\d{4}', data)
 
-        with open('output.txt', 'r') as file:
-            data = file.read()
-            number_plate = re.findall('[A-Z]{2}\\d{2}[A-Z]{2}\\d{4}', data)
+    user_name = re.sub('@.*','',email)
 
-        user_name = re.sub('@.*','',email)
+    # Get a reference to the 'number_plates' node in the database
+    ref = db.reference().child(user_name).child('pending')
 
-        # Get a reference to the 'number_plates' node in the database
-        ref = db.reference().child(user_name).child('pending')
+    # Store number plates and video metadata in Realtime Database
+    timestamps = ref.get()
+    data = ','.join(str(n) for n in set(number_plate))
 
-        number_plate_ref = ref.child('number_plates')
-        data = ','.join(str(n) for n in set(number_plate))
-        number_plate_ref.set(data)
-        timestamps = ref.get()
+    for timestamp in timestamps:
+        timestamp_ref = ref.child(timestamp)
+        if 'number_plates' not in timestamp_ref.get():
+            # Upload video to Firebase Storage
+            bucket = storage.bucket()
+            print(str(timestamp))
+            file_name = f'{user_name}_{str(timestamp)}.mp4'
+            with open('assets/uploaded_video.mp4', 'rb') as video_file:
+                video_blob = bucket.blob(file_name)
+                video_blob.upload_from_file(video_file)
+                video_blob.make_public()
 
-        # Save video to Firestore
-        with open('assets/uploaded_video.mp4', 'rb') as video_file:
-            video_data = video_file.read()
-        
-        # save video to firestore
-        firestore_db = firestore.client()
-        video_ref = firestore_db.collection(u'videos').document()
-        
-        file_name = f'{user_name}_{video_ref.id}.mp4'
-        local_file = 'assets/uploaded_video.mp4'
-        bucket = storage.bucket()
-        blob = bucket.blob(file_name)
-        blob.upload_from_filename(local_file)
-        blob.make_public()
+            # Get the download URL of the video file from Firebase Storage
+            video_url = video_blob.public_url
+            timestamp_ref.update({
+                'number_plates': data,
+                'video_name': file_name,
+                'video_url': video_url
+            })
 
-        # video_ref.set({
-        #     u'video_data': firebase_admin.firestore.Blob(video_data),
-        #     u'video_name': file_name
-        # })
-
-        # Get the download URL of the video file from Firestore
-        # video_url = video_ref.get().get('video_name')
-
-        # Iterate over each key in the timestamps dictionary and check if it has a child 'number_plates'
-        for timestamp in timestamps:
-            timestamp_ref = ref.child(timestamp)
-            if not timestamp_ref.get().get('number_plates'):
-                number_plate_ref = timestamp_ref.child('number_plates')
-                data = ','.join(str(n) for n in set(number_plate))
-                number_plate_ref.set(data)
-                timestamp_ref.child("uploaded_video").set(blob.public_url)
-                break
-
-        return "Data stored successfully", 200
-    except Exception as e:
-        print(f"{e.args}")
+    return "Data stored successfully", 200
