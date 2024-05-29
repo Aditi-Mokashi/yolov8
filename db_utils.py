fb3e36ff821f7888ad9a98c66c3a38c567863615
Aditi-Mokashi <aditimokashi23@gmail.com>
2024-05-30 02:01:41 +0530

    feat: get only plates with valid state code

--- db_utils.py
+++ db_utils.py
@@ -10,9 +10,15 @@ def insert_to_realtime_db(email: str):
         'storageBucket': 'eqw-violationeye-42382.appspot.com'
     })
 
+    # Define the state codes for India
+    state_codes = 'AP,AR,AS,BR,CG,GA,GJ,HR,HP,JH,KA,KL,MP,MH,MN,ML,MZ,NL,OD,PB,RJ,SK,TN,TG,TR,UP,UK,WB,AN,CH,DN,DL,LD,PY'  # Update this with the desired state codes
+    
+    # Modify the regex pattern
+    pattern = f'[{state_codes}]{{2}}\\d{{2}}[A-Z]{{2}}\\d{{4}}'
+
     with open('output.txt', 'r') as file:
         data = file.read()
-        number_plate = re.findall('[A-Z]{2}\\d{2}[A-Z]{2}\\d{4}', data)
+        number_plate = re.findall(pattern, data)
 
     user_name = re.sub('@.*','',email)
 
@@ -28,7 +34,6 @@ def insert_to_realtime_db(email: str):
         if 'number_plates' not in timestamp_ref.get():
             # Upload video to Firebase Storage
             bucket = storage.bucket()
-            print(str(timestamp))
             file_name = f'{user_name}_{str(timestamp)}.mp4'
             with open('assets/uploaded_video.mp4', 'rb') as video_file:
                 video_blob = bucket.blob(file_name)
